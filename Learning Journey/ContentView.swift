1d11d81f8d1e5689f33ad88f2ca85348a012c4d0
Tala Aldhahri <toti_kd123@hotmail.com>
2025-10-21 15:00:13 +0300

    Initial Commit

---
+++ Learning Journey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning Journey
+//
+//  Created by Tala Aldhahri on 29/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Tala Aldhahri on 29/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
